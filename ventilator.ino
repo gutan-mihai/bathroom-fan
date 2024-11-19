2639b13830175a2e403ff8f666d0ed8e07a308f9
Gutan Mihai <gutan.mihai@gmail.com>
2024-11-19 23:15:02 +0200

    removed debug code

--- ventilator.ino
+++ ventilator.ino
@@ -2,7 +2,7 @@
 #define LOW_POWER_PIN 5
 #define MAX_POWER_PIN 4
 const unsigned long humidity_read_interval = 5000; // 5 seconds
-const unsigned long reset_dimmer_power_intervall = 600000; // 10 minutes
+const unsigned long reset_dimmer_power_interval = 600000; // 10 minutes
 unsigned long humidity_readed_time = 0;
 unsigned long low_humidity_detect_time = 0;
 bool is_waiting_humidity_changed = false;
@@ -18,8 +18,6 @@ int low_pin_value = LOW;
 int max_pin_value = HIGH;
 
 void setup() {
-  Serial.begin(9600);
-  
   // Setup SSR (Solid State Relay)
   pinMode(LOW_POWER_PIN, OUTPUT);
   pinMode(MAX_POWER_PIN, OUTPUT);
@@ -33,16 +31,14 @@ void setup() {
 void loop() {
   // Read humidity
   float relative_humidity = 0;
-  if (millis() - humidity_readed_time > humidity_read_interval) {
+  unsigned long current_millis = millis();
+  if (current_millis - humidity_readed_time >= humidity_read_interval) {
     relative_humidity = dht.readHumidity();
     if (isnan(relative_humidity)) {
       relative_humidity = 0;
-      Serial.println(F("Error reading humidity!"));
-    } else {
-      Serial.print(F("Humidity: ")); Serial.print(relative_humidity); Serial.println(F("%"));
     }
 
-    humidity_readed_time = millis();
+    humidity_readed_time = current_millis;
   }
 
   // Calculate new relay states
@@ -53,11 +49,11 @@ void loop() {
     max_value = LOW;
   } else {
     if (!is_waiting_humidity_changed && max_pin_value == LOW) {
-      low_humidity_detect_time = millis();
+      low_humidity_detect_time = current_millis;
       is_waiting_humidity_changed = true;
     }
 
-    if (millis() - low_humidity_detect_time > reset_dimmer_power_intervall) {
+    if (current_millis - low_humidity_detect_time >= reset_dimmer_power_interval) {
       low_value = LOW;
       max_value = HIGH;
       is_waiting_humidity_changed = false;
@@ -67,13 +63,11 @@ void loop() {
   // Write relay states
   if (low_pin_value != low_value) {
     low_pin_value = low_value;
-    Serial.print(F("new low_pin_value: ")); Serial.println(low_pin_value);
     digitalWrite(LOW_POWER_PIN, low_pin_value);
   }
 
   if (max_pin_value != max_value) {
     max_pin_value = max_value;
-    Serial.print(F("new max_pin_value: ")); Serial.println(max_pin_value);
     digitalWrite(MAX_POWER_PIN, max_pin_value);
   }
 
